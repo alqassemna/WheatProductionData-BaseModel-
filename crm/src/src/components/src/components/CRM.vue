229aad352971fc41df3c519168f82398b260647d
alqassemna <136648352+alqassemna@users.noreply.github.com>
2025-06-22 01:02:16 -0400

    Create CRM.vue

---
+++ crm/src/src/components/src/components/CRM.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Stakeholder CRM</h2>
+    <StakeholderList :stakeholders="stakeholders" />
+    <!-- Add forms to add/edit stakeholders -->
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import StakeholderList from './StakeholderList.vue'
+
+const stakeholders = ref([])
+
+onMounted(async () => {
+  const res = await axios.get('/crm/stakeholders')
+  stakeholders.value = res.data
+})
+</script>
