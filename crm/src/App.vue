23b8065a7adfa93f4eca4e21f7ad6741477ebca1
alqassemna <136648352+alqassemna@users.noreply.github.com>
2025-06-22 01:00:52 -0400

    Create App.vue

---
+++ crm/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <Dashboard />
+    <CRM />
+    <ImpactPathway />
+  </div>
+</template>
+
+<script setup>
+import Dashboard from './components/Dashboard.vue'
+import CRM from './components/CRM.vue'
+import ImpactPathway from './components/ImpactPathway.vue'
+</script>
