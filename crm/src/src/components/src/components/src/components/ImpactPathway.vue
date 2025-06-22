9ed19e290e21d0383d57c66948ce1324bb2ff23f
alqassemna <136648352+alqassemna@users.noreply.github.com>
2025-06-22 01:02:48 -0400

    Create ImpactPathway.vue

---
+++ crm/src/src/components/src/components/src/components/ImpactPathway.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Impact Pathway</h2>
+    <!-- Visualize nodes/edges as a graph (use vis.js, cytoscape, etc.) -->
+    <ImpactGraph :nodes="nodes" :edges="edges" />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import ImpactGraph from './ImpactGraph.vue'
+
+const nodes = ref([])
+const edges = ref([])
+
+onMounted(async () => {
+  nodes.value = (await axios.get('/impact/pathway/nodes')).data
+  edges.value = (await axios.get('/impact/pathway/edges')).data
+})
+</script>
